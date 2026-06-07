b4e9002bdcc94c3023daca979a7a0bc41d2476d9
Marie <104371913+MistressOfDNS@users.noreply.github.com>
2026-06-07 22:08:53 +0200

    Name build jars for Modrinth releases

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,11 @@ plugins {
     java
 }
 
+val pluginVersion = "1.0"
+val paperTargetVersion = "1.21.8"
+
 group = "dev.zitrone"
-version = "1.21.8"
+version = pluginVersion
 
 repositories {
     mavenCentral()
@@ -25,8 +28,11 @@ tasks.withType<JavaCompile>().configureEach {
     options.release = 21
 }
 
+tasks.jar {
+    archiveFileName.set("PluginHider-$pluginVersion-Paper-$paperTargetVersion.jar")
+}
+
 tasks.processResources {
-    val pluginVersion = project.version.toString()
     inputs.property("version", pluginVersion)
 
     filesMatching("plugin.yml") {
